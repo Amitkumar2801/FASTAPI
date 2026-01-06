5fc9ad4c64680a609cbc923b439d85a987002c07
Amitkumar2801 <amitkumar.arwal28@gmail.com>
2026-01-06 13:49:18 +0530

    Add about endpoint with personal introduction

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "hello world"}
+
+@app.get("/about")
+def about():
+    return {"Hello My Name Is AMIT KUMAR SHARMA , iam pursuning diploma in artificial intellingence and machine leraning from Government Polytechnic Barh,Patna "}
