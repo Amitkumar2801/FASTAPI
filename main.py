1a0e20ad0792c6ef67b748e1ea0ffb946f26da6c
Amitkumar2801 <amitkumar.arwal28@gmail.com>
2026-01-10 21:44:28 +0530

    Fix formatting by adding a newline after the contact endpoint in FastAPI application

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def about():
 @app.get("/contact")
 def contect():
     return {"Email": "amitkumarsharma12345@gmail.com"}
+
 @app.get("/hobby")
 def hobby():
     return {"My Hobbies are Playing Cricket , Reading Books and Watching Movies"}
