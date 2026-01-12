3975ce5747263c45033608a8f68a08f349ff94c4
Amitkumar2801 <amitkumar.arwal28@gmail.com>
2026-01-12 22:18:05 +0530

    Remove education endpoint from FastAPI application

--- main.py
+++ main.py
@@ -20,7 +20,3 @@ def hobby():
 @app.get("/skills")
 def skills():
     return {"My Skills are Python, C++, HTML, CSS, JavaScript"}
-
-@app.get("/education")
-def education():
-    return {"I am pursuning diploma in artificial intellingence and machine leraning from Government Polytechnic Barh,Patna"}
