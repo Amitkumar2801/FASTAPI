70ec49029b3e92efe120ded4a8e1e3c998eb8e48
Amitkumar2801 <amitkumar.arwal28@gmail.com>
2026-01-14 23:49:23 +0530

    new api  load data

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+import json
+
+def load_data9():
+    with open('patient.json', 'r') as f:
+       data = json.load(f)
+       return data
+
 @app.get("/")
 def hello():
     return {'message': 'patient Management system API'}
