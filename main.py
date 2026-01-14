0222e5c3b0b59ec943a62cded5fe76ef6447abc4
Amitkumar2801 <amitkumar.arwal28@gmail.com>
2026-01-14 23:25:30 +0530

    new code

--- main.py
+++ main.py
@@ -10,9 +10,5 @@ def about():
     return {"Hello My Name Is AMIT KUMAR SHARMA , iam pursuning diploma in artificial intellingence and machine leraning from Government Polytechnic Barh,Patna "}
 
 @app.get("/contact")
-def contect():
+def contact():
     return {"Email": "amitkumarsharma12345@gmail.com"}
-
-@app.get("/hobby")
-def hobby():
-    return {"My Hobbies are Playing Cricket , Reading Books and Watching Movies"}
